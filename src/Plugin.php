d0cd197247206c6300ddee3ab870d192ec85e30a
Narendra Vaghela <narendra@sprintcube.com>
2018-09-24 17:53:19 +0530

    Initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * SendGrid Plugin for CakePHP 3
+ * Copyright (c) SprintCube (https://www.sprintcube.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.md
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) SprintCube (https://www.sprintcube.com)
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @link      https://github.com/sprintcube/cakephp-sendgrid
+ * @since     1.0.0
+ */
+
+namespace SendGrid;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for SendGrid
+ */
+class Plugin extends BasePlugin
+{
+}
