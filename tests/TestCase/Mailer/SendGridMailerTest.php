435d658b2070defa8c8fc0a58dec0c7d816c8da9
Narendra Vaghela <narendra@sprintcube.com>
2022-04-09 17:27:11 +0530

    Add more files/dir to gitignore

---
+++ tests/TestCase/Mailer/SendGridMailerTest.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace SendGrid\Test\TestCase\Mailer;
+
+use Cake\TestSuite\TestCase;
+use SendGrid\Mailer\SendGridMailer;
+
+/**
+ * SendGrid\Mailer\SendGridMailer Test Case
+ */
+class SendGridMailerTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \SendGrid\Mailer\SendGridMailer
+     */
+    protected $SendGridMailer;
+
+    /**
+     * Test __construct method
+     *
+     * @return void
+     * @uses \SendGrid\Mailer\SendGridMailer::__construct()
+     */
+    public function testConstruct(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test setTemplate method
+     *
+     * @return void
+     * @uses \SendGrid\Mailer\SendGridMailer::setTemplate()
+     */
+    public function testSetTemplate(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test setSendAt method
+     *
+     * @return void
+     * @uses \SendGrid\Mailer\SendGridMailer::setSendAt()
+     */
+    public function testSetSendAt(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
