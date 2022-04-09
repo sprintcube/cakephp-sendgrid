a397473f91583349345d4ffbc79997e76641c842
Narendra Vaghela <narendra@sprintcube.com>
2022-04-09 17:58:35 +0530

    Add use statement

--- tests/TestCase/Mailer/SendGridMailerTest.php
+++ tests/TestCase/Mailer/SendGridMailerTest.php
@@ -6,6 +6,7 @@ namespace SendGrid\Test\TestCase\Mailer;
 
 use Cake\Mailer\TransportFactory;
 use Cake\TestSuite\TestCase;
+use SendGrid\Mailer\SendGridMailer;
 
 /**
  * SendGrid\Mailer\SendGridMailer Test Case
