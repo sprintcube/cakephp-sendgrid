bb9505e0f51e3e31a9fd82dfc70b065b6b9a3a13
Narendra Vaghela <narendra@sprintcube.com>
2025-01-10 16:41:39 +0530

    Update doc

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,10 +1,23 @@
 <?php
+
 declare(strict_types=1);
+
+/**
+ * SendGrid Plugin for CakePHP
+ * Copyright (c) SprintCube (https://www.sprintcube.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.md
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) SprintCube (https://www.sprintcube.com)
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @link      https://github.com/sprintcube/cakephp-sendgrid
+ * @since     5.0.0
+ */
 
 namespace SendGrid\Controller;
 
 use App\Controller\AppController as BaseController;
 
-class AppController extends BaseController
-{
-}
+class AppController extends BaseController {}
